dba915574eff875c53d815a3efa2398e23012ae6
ProgrammerJM <johnmarktizado@gmail.com>
2023-08-30 12:50:51 +0800

    Minified Script JS

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxt-j-GxlaXwW3WnwgASKytf8T5qaI0GS6kFoZpk29-4afESHPJP1nub7mQrL9PWA0/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById('msg')
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { 
-        method: "POST", body: new FormData(form)
-    })
-        .then(response => {
-            msg.innerHTML = "Welcome To ICHEM Library!"
-            setTimeout(function() {
-                msg.innerHTML = ""
-        }, 1000)
-        form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
-})
+const scriptURL="https://script.google.com/macros/s/AKfycbxt-j-GxlaXwW3WnwgASKytf8T5qaI0GS6kFoZpk29-4afESHPJP1nub7mQrL9PWA0/exec",form=document.forms["submit-to-google-sheet"],msg=document.getElementById("msg");form.addEventListener("submit",a=>{a.preventDefault(),fetch(scriptURL,{method:"POST",body:new FormData(form)}).then(()=>{msg.innerHTML="Welcome To ICHEM Library!",setTimeout(function(){msg.innerHTML=""},1e3),form.reset()}).catch(a=>console.error("Error!",a.message))});
